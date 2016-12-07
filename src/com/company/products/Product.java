e9be45983dc5aba75d8186d74346ffc7e11d0ca8
Daniilys <daniil.pozhidaev@gmail.com>
2016-12-07 12:02:22 +0200

    Refactor product classes

--- src/com/company/products/Product.java
+++ src/com/company/products/Product.java
@@ -3,12 +3,22 @@ package com.company.products;
 /**
  * Created by mahasamatman on 29/11/16.
  */
-public abstract class Product {
+public class Product {
 
 	private final String code;
 	private final float price;
 
-	protected Product(String code, float price) {
+	protected Product(final String code, final float price) {
+		if (code == null) {
+			throw new NullPointerException("Product code can't be null");
+		} else if (code.isEmpty()) {
+			throw new IllegalArgumentException("Product code can't be empty");
+		}
+
+		if (price < 0) {
+			throw new IllegalArgumentException("Product price can't be negative");
+		}
+
 		this.code = code;
 		this.price = price;
 	}
--- src/com/company/products/ProductsFabric.java
+++ src/com/company/products/ProductsFabric.java
@@ -1,22 +1,32 @@
 package com.company.products;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mahasamatman on 29/11/16.
  */
 public class ProductsFabric {
+	public static final String APPLE_JUICE_CODE = "AJ";
+	public static final String COFFEE_CODE = "CF";
+	public static final String FRUIT_TEA_CODE = "FR";
+	public static final String STRAWBERRIES_CODE = "SR";
+
+	private Map<String, Float> productPriceMap = new HashMap<>();
+
+	public ProductsFabric() {
+		productPriceMap.put(APPLE_JUICE_CODE, 7.25f);
+		productPriceMap.put(COFFEE_CODE, 11.23f);
+		productPriceMap.put(FRUIT_TEA_CODE, 3.11f);
+		productPriceMap.put(STRAWBERRIES_CODE, 5.0f);
+	}
 
 	public Product create(final String productCode) {
-		switch (productCode) {
-			case AppleJuice.CODE :
-				return new AppleJuice();
-			case Coffee.CODE :
-				return new Coffee();
-			case FruitTea.CODE :
-				return new FruitTea();
-			case Strawberries.CODE :
-				return new Strawberries();
-			default:
-				throw new IllegalArgumentException("No product found with that code");
+		if (productPriceMap.containsKey(productCode)) {
+			float productPrice = productPriceMap.get(productCode);
+			return new Product(productCode, productPrice);
+		} else {
+			throw new IllegalArgumentException("No product found with that code");
 		}
 	}
 }
